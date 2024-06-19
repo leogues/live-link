634c85ae234515d76908d1dce867a44ede006fc5
Leonardo Rodrigues <leo_rodrigues1999@hotmail.com>
2024-06-19 14:17:40 -0300

    chore: Update GoogleButton and LoginForm styles for responsiveness

--- client/src/components/GoogleButton.tsx
+++ client/src/components/GoogleButton.tsx
@@ -7,7 +7,7 @@ export const GoogleButton: React.FC<{ onClick: () => void }> = ({
   return (
     <Button
       onClick={onClick}
-      className="flex items-center gap-3 border border-gray-400 px-6 py-3 font-bold hover:bg-neutral-200 dark:border-darkBlue-600 dark:hover:bg-darkBlue-600"
+      className="flex items-center gap-1 border border-gray-400 px-6 py-3 font-bold hover:bg-neutral-200 dark:border-darkBlue-600 dark:hover:bg-darkBlue-600 sm:gap-3"
     >
       <img src={GoogleIcon} />
       <span>Continue com o Google</span>
