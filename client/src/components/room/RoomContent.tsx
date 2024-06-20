f1ceee0b52281abd188541c5cb528ce7a74d4488
Leonardo Rodrigues <leo_rodrigues1999@hotmail.com>
2024-06-20 20:10:52 -0300

    fix: backdrop opacity in light mode

--- client/src/components/room/RoomContent.tsx
+++ client/src/components/room/RoomContent.tsx
@@ -9,7 +9,7 @@ export const RoomContent: React.FC = () => {
   return (
     <>
       {chat.isChatOpen && (
-        <div className="absolute inset-0 z-30 block h-screen w-full bg-black bg-opacity-20 md:hidden"></div>
+        <div className="absolute inset-0 z-30 block h-screen w-full bg-black bg-opacity-0 dark:bg-opacity-20 md:hidden"></div>
       )}
       <div className="flex flex-col overflow-hidden">
         <div className="relative flex h-full">
